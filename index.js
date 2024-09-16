83603ec795b465727b0089efa9648c10a9c85f91
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-09-16 10:21:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// const money = 44;
+
+// if(money > 56){
+// console.log('i eating burger')
+// }else{
+//     console.log('i not eating now')
+// }
+
+const number = [23,43, 234,21, 89, 23];
+console.log(number[3])
+console.log(number.indexOf(234))
---
+++ loop.js
@@ -0,0 +1,11 @@
+const number = [75,64,67,89,12,23,65,58];
+console.log(number);
+for(num of number){
+    console.log(num)
+}
+
+const flower = ['joba', 'rojoni', 'gonda','murukjoba', 'beli','gass full','golap'];
+console.log(flower);
+for(flow of flower){
+    console.log(flow)
+}
