d7564d46dcdce7af8450937f50dbac5fa8021b10
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-09-18 13:04:46 +0600

    function

---
+++ function.js
@@ -0,0 +1,44 @@
+function amiHotas(){
+    console.log('or a jibon ses');
+    console.log('vlo hoya jaw masud');
+}
+
+function mountain(){
+    console.log('akhon o kno mone pore januyar kobis take')
+}
+
+// amiHotas();
+// mountain();
+
+function canI(sum1, sum2){
+    const ttl = sum1 + sum2;
+    console.log(ttl)
+
+}
+
+// canI(2, 4);
+
+function run(bat1, bat2){
+    total=bat1 + bat2;
+    console.log(bat1, bat2, 'total run is', total)
+}
+
+const tamim = 44;
+const sakib = 12;
+
+// run(tamim, sakib);
+
+function tenSomoi(number){
+    const double = number * 2;
+    return double;
+}
+
+const result = tenSomoi(9);
+// console.log(result);
+
+function isEven(sum){
+    if(sum%2 === 0){
+        return true;
+    } return false;
+}
+console.log(isEven(82))
